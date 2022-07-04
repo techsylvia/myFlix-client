a26af0f454428cdcc39ecaa9f9d01be6f33da3fe
Sylvia <sylvia@donchev.is>
2022-07-04 09:28:17 +0200

    code updated render main-vies

--- src/components/main-view.jsx
+++ src/components/main-view.jsx
@@ -29,11 +29,16 @@ class MainView extends React.Component {
     };
   }
   render() {
+    const { movies } = this.state;
+
+    if (movies.length === 0)
+      return <div className="main-view">The list is empty</div>;
+
     return (
       <div className="main-view">
-        <div>Inception</div>
-        <div>The Shawshank Redemption</div>
-        <div>Gladiator</div>
+        {movies.map((movie) => (
+          <MovieCard />
+        ))}
       </div>
     );
   }
