eca53e6bac7796a508e67971f41fd50170c76d92
Sylvia <sylvia@donchev.is>
2022-07-04 09:03:55 +0200

    export default

--- src/components/main-view.jsx
+++ src/components/main-view.jsx
@@ -1,13 +1,17 @@
 import React from " react";
 
-export class MainView extends React.Component {
+import React from "react";
+
+class MainView extends React.Component {
   render() {
     return (
       <div className="main-view">
         <div>Inception</div>
-        <div>The Shawsank Redemption</div>
+        <div>The Shawshank Redemption</div>
         <div>Gladiator</div>
       </div>
     );
   }
 }
+
+export default MainView;
