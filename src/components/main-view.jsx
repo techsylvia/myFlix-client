c4a0f93445b8f476a115faa4757fd2464ddd0457
Sylvia <sylvia@donchev.is>
2022-07-04 09:07:58 +0200

    id's added

--- src/components/main-view.jsx
+++ src/components/main-view.jsx
@@ -3,6 +3,31 @@ import React from " react";
 import React from "react";
 
 class MainView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [
+        {
+          _id: 1,
+          Title: "Inception",
+          Description: "des c1...",
+          ImagePath: "...",
+        },
+        {
+          _id: 2,
+          Title: "The Shawshank Redemption",
+          Description: "desc2...",
+          ImagePath: "...",
+        },
+        {
+          _id: 3,
+          Title: "Gladiator",
+          Description: "desc3...",
+          ImagePath: "...",
+        },
+      ],
+    };
+  }
   render() {
     return (
       <div className="main-view">
