df58b9a08e537e70b28e461d4210e8fc4aa8f701
Sylvia <sylvia@donchev.is>
2022-07-12 21:29:36 +0200

    const username err & passworderr

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -2,66 +2,69 @@ import React from "react";
 import React, { useState } from "react";
 
 export function LoginView(props) {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+   const [usernameErr, setUsernameErr] = useState("");
+  const [passwordErr, setPasswordErr] = useState("");
+  
+  export class LoginView extends React.Component {
+    constructor(props) {
+      super(props);
 
-export class LoginView extends React.Component {
-  constructor(props) {
-    super(props);
+      this.state = {
+        username: "",
+        password: "",
+      };
 
-    this.state = {
-      username: "",
-      password: "",
-    };
+      this.onUsernameChange = this.onUsernameChange.bind(this);
+      this.onPasswordChange = this.onPasswordChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    this.onUsernameChange = this.onUsernameChange.bind(this);
-    this.onPasswordChange = this.onPasswordChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+    onUsernameChange(event) {
+      this.setState({
+        username: event.target.value,
+      });
+    }
 
-  onUsernameChange(event) {
-    this.setState({
-      username: event.target.value,
-    });
-  }
+    onPasswordChange(event) {
+      this.setState({
+        password: event.target.value,
+      });
+    }
 
-  onPasswordChange(event) {
-    this.setState({
-      password: event.target.value,
-    });
-  }
-
-  handleSubmit() {
-    const { username, password } = this.state;
+   const handleSubmit = (e) => {
+    e.preventDefault();
     console.log(username, password);
     /* Send a request to the server for authentication */
-    /* then call this.props.onLoggedIn(username) */
-    // this.props.onLoggedIn(username);
-  }
+    /* then call props.onLoggedIn(username) */
+    // props.onLoggedIn(username);
+  };
 
-  render() {
-    return (
-      <form>
-        <label>
-          Username:
-          <input
-            type="text"
-            value={this.state.username}
-            onChange={this.onUsernameChange}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={this.state.password}
-            onChange={this.onPasswordChange}
-          />
-        </label>
-        <button type="button" onClick={this.handleSubmit}>
-          Submit
-        </button>
-      </form>
-    );
+    render() {
+      return (
+        <form>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={this.state.username}
+              onChange={this.onUsernameChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={this.onPasswordChange}
+            />
+          </label>
+          <button type="button" onClick={this.handleSubmit}>
+            Submit
+          </button>
+        </form>
+      );
+    }
   }
 }
