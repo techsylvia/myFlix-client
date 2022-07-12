141c9cf63a92b61258ab191a833f3e4aa330b7b0
Sylvia <sylvia@donchev.is>
2022-07-12 21:41:39 +0200

    button

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -4,36 +4,10 @@ import React, { useState } from "react";
 export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-   const [usernameErr, setUsernameErr] = useState("");
+  const [usernameErr, setUsernameErr] = useState("");
   const [passwordErr, setPasswordErr] = useState("");
-  
-  export class LoginView extends React.Component {
-    constructor(props) {
-      super(props);
 
-      this.state = {
-        username: "",
-        password: "",
-      };
-
-      this.onUsernameChange = this.onUsernameChange.bind(this);
-      this.onPasswordChange = this.onPasswordChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    onUsernameChange(event) {
-      this.setState({
-        username: event.target.value,
-      });
-    }
-
-    onPasswordChange(event) {
-      this.setState({
-        password: event.target.value,
-      });
-    }
-
-     const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     /* console.log(username, password);
     props.onLoggedIn(username); */
@@ -55,30 +29,27 @@ export function LoginView(props) {
     }
   };
 
-    render() {
-      return (
-        <form>
-          <label>
-            Username:
-            <input
-              type="text"
-              value={this.state.username}
-              onChange={this.onUsernameChange}
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              type="password"
-              value={this.state.password}
-              onChange={this.onPasswordChange}
-            />
-          </label>
-          <button type="button" onClick={this.handleSubmit}>
-            Submit
-          </button>
-        </form>
-      );
-    }
-  }
+  return (
+    <form>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={this.state.username}
+          onChange={this.onUsernameChange}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={this.onPasswordChange}
+        />
+      </label>
+      <Button className="" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </form>
+  );
 }
