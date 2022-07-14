8dd0e2afc1bf58fd0a9a779e1a3875e1aa2272b8
Sylvia <sylvia@donchev.is>
2022-07-14 19:41:07 +0200

    import prop types

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 export function LoginView(props) {
   const [username, setUsername] = useState("");
