9dc8741a7dd9e77de3a00e77e6c523b4eb0325c3
Sylvia <sylvia@donchev.is>
2022-07-14 22:00:29 +0200

    props added & handleSubmit

--- src/RegistrationView/registration-view.jsx
+++ src/RegistrationView/registration-view.jsx
@@ -1,1 +1,14 @@
 import React, { useState } from "react";
+//user registration form taking necessary user details
+export function RegistrationView(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(username, password, email, birthday);
+  props.onRegister(false);
+};
