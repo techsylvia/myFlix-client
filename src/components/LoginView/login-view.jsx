a1b29f911e2d30d6d35160cc8f5b227c9a6ff162
Sylvia <sylvia@donchev.is>
2022-07-14 19:40:18 +0200

    loginview proptype added

--- src/components/LoginView/login-view.jsx
+++ src/components/LoginView/login-view.jsx
@@ -53,3 +53,11 @@ export function LoginView(props) {
     </form>
   );
 }
+
+LoginView.propTypes = {
+  user: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }),
+  onLoggedIn: PropTypes.func.isRequired,
+};
