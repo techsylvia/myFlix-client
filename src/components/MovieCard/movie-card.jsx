7dbdb56b62de45e372e00e59a36b428730b816a9
Sylvia <sylvia@donchev.is>
2022-08-11 15:56:44 +0200

    scss added

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card, CardGroup, Container, Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import './movie-card.scss'
 
 export class MovieCard extends React.Component {
   render() {
