6d6aa405ec1c7fd07c809ebb8c62bfb7782e213d
Sylvia <sylvia@donchev.is>
2022-08-14 08:29:34 +0200

    add form validation to registration view

--- src/components/RegistrationView/registration-view.jsx
+++ src/components/RegistrationView/registration-view.jsx
@@ -1,32 +1,68 @@
 import { Form, Button, Card, CardGroup, Container, Col, Row } from 'react-bootstrap';
 import React, { useState } from "react";
 import axios from "axios";
+import { useHistory } from 'react-router-dom';
 
 //user registration form taking necessary user details
 export function RegistrationView() {
+  const history = useHistory()
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
   const [birthday, setBirthday] = useState("");
+  const [values, setValues] = useState({
+    usernameError: '',
+    passwordError: '',
+    emailError: '',
+    birthdayError: ''
+  })
+
+  const isValid = () => {
+    if (!username) {
+      setValues({ ...values, usernameError: 'Username Required' });
+    } else if (username.length < 2) {
+      setValues({ ...values, usernameError: 'Username must be 2 or more characters long' });
+    }
+    if (!password) {
+      setValues({ ...values, passwordError: 'Password is required.' });
+    } else if (password.length < 6) {
+      setValues({ ...values, passwordError: 'Password must be 6 or more characters long' });
+    }
+    if (!email) {
+      setValues({ ...values, emailError: 'Please use a valid email' });
+    } else if (email.indexOf('@') === -1) {
+      setValues({ ...values, emailErr: 'Please use a valid email' });
+    }
+    if (!birthday) {
+      setValues({ ...values, birthdayError: 'Birthdate required' });
+    }
+
+    return Object.values(values).every(el => el === '');
+  }
 
   const handleSubmit = (e) => {
+    console.log("handleSubmit");
     e.preventDefault();
-    console.log(username, password, email, birthday);
-    axios
-      .post("https://sylvmovieapp.herokuapp.com/users", {
-        Username: username,
-        Password: password,
-        Email: email,
-        Birthday: birthday,
-      })
-      .then((response) => {
-        console.log(response.data)
-        alert("Registration successful, please login!");
-        window.open('/', '_self');
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    if (isValid()) {
+      console.log('registration form is valid');
+      axios
+        .post("https://sylvmovieapp.herokuapp.com/users", {
+          Username: username,
+          Password: password,
+          Email: email,
+          Birthday: birthday,
+        })
+        .then((response) => {
+          console.log(response.data)
+          alert("Registration successful, please login!");
+          history.push('/')
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    console.log(Object.values(values))
   };
 
   return (
@@ -46,7 +82,11 @@ export function RegistrationView() {
                       onChange={e => setUsername(e.target.value)}
                       placeholder='Enter a username'
                       required
+                      isInvalid={values.usernameError !== ''}
                     />
+                    <Form.Control.Feedback type="invalid">
+                      {values.usernameError}
+                    </Form.Control.Feedback>
                   </Form.Group>
 
                   <Form.Group className="mb-3">
@@ -55,10 +95,14 @@ export function RegistrationView() {
                       type='password'
                       value={password}
                       onChange={e => setPassword(e.target.value)}
-                      minLength='4'
-                      placeholder='Your password must be 4 or more characters'
+                      minLength='6'
+                      placeholder='Your password must be 6 or more characters'
                       required
+                      isInvalid={values.passwordError !== ''}
                     />
+                    <Form.Control.Feedback type="invalid">
+                      {values.passwordError}
+                    </Form.Control.Feedback>
                   </Form.Group>
 
                   <Form.Group className="mb-3">
@@ -67,7 +111,11 @@ export function RegistrationView() {
                       type='date'
                       value={birthday}
                       onChange={e => setBirthday(e.target.value)}
+                      isInvalid={values.birthdayError !== ''}
                     />
+                    <Form.Control.Feedback type="invalid">
+                      {values.birthdayError}
+                    </Form.Control.Feedback>
                   </Form.Group>
 
                   <Form.Group className="mb-3">
@@ -78,7 +126,11 @@ export function RegistrationView() {
                       onChange={e => setEmail(e.target.value)}
                       placeholder='Enter a valid Email address'
                       required
+                      isInvalid={values.emailError !== ''}
                     />
+                    <Form.Control.Feedback type="invalid">
+                      {values.emailError}
+                    </Form.Control.Feedback>
                   </Form.Group>
 
                   <Button variant='warning'
